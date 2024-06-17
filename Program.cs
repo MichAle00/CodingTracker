db43189654a7360ef50a99ec04c11928e7362399
Miguel Alejandro Madrigal Cabeza <mmadrigalcabeza@gmail.com>
2024-06-17 10:05:32 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SQLite;
+using Spectre.Console;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace coding_tracker
+{
+    class CodingTracker
+    {
+        static string? connectionString = @"data-source=C:\Users\mmadr\Desktop\Escuela\c#\CodingTracker\coding-tracker.db";
+        static void Main()
+        {
+            
+        }
+    }
+}
---
+++ UserInput.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTracker
+{
+    internal class UserInput
+    {
+        public UserInput()
+        {
+            Console.Clear();
+            bool closeApp = true;
+
+            while (closeApp)
+            {
+                Console.WriteLine("Main Menu:");
+                Console.WriteLine("What do you like to do?");
+                Console.WriteLine("Type 0 to Close Application");
+                Console.WriteLine("Type 1 to View All Records");
+                Console.WriteLine("Type 2 to Insert Record");
+                Console.WriteLine("Type 3 to Delete Record");
+                Console.WriteLine("Type 4 to Update Record");
+                Console.WriteLine("--------------------------------------\n");
+
+                string? command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "0":
+                        break;
+
+                    case "1":
+                        break;
+
+                    case "2":
+                        break;
+
+                    case "3":
+                        break;
+
+                    case "4":
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
